393b7cbeaee5e2a7f3c86da867d527b59df3e547
Rogerio Castro <rogeriomunizcastro@gmail.com>
2025-09-08 20:13:17 -0300

    fix baseurl and vendor folder on backend

--- frontend/package/src/services/api.ts
+++ frontend/package/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost/api', // Adjust if your backend URL is different
+  baseURL: '/api', // Adjust if your backend URL is different
   
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
